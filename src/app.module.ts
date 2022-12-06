87f131414c85ad612fca66765acf9140767e7993
Vittor Daniel <vittordaniel1108@gmail.com>
2022-12-06 20:51:58 -0300

    initi login

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { ActiveAccountModule } from './modules/active-account/active-account.module';
 import { ResetPasswordModule } from './modules/reset-password/reset-password.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule, ActiveAccountModule, ResetPasswordModule],
+  imports: [UserModule, ActiveAccountModule, ResetPasswordModule, AuthModule],
   controllers: [],
   providers: [],
 })
