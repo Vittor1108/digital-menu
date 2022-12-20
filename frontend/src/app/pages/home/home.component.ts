1fab95ce66836f873b9356a653359ba7a40d4f06
Vittor Daniel <vittordaniel1108@gmail.com>
2022-12-19 22:43:11 -0300

    iniit response

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  public showAccordion = (numberAccordion: number): void => {
+    const accordions = document.querySelectorAll<HTMLElement>('.accordion');
+    accordions[numberAccordion].classList.toggle('show');
+    accordions.forEach((e, index) => {
+      if (index === numberAccordion) {
+        return;
+      }
 
+      e.classList.remove('show');
+    });
+  };
+
+  public toggleMenu = (): void => {
+    const menu = document.querySelector<HTMLElement>('.primarySideBar');
+    const barsMenu = document.querySelectorAll<HTMLElement>('.menu > span');
+    barsMenu.forEach((e) => {
+      e.classList.toggle('menuFull');
+    });
+    const main = document.querySelector<HTMLElement>('main');
+    main?.classList.toggle('mainFullScreen');
+    menu?.classList.toggle('toggleMenu');
+  };
 }
