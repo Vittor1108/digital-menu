1c228d918be5289e9cf1cca1d8ad0d8f437fde69
Vittor Daniel <vittor.daniel@desenvolversoftware.com.br>
2022-12-25 23:22:27 -0300

    init service categories

--- frontend/src/app/pages/product/category/category.component.ts
+++ frontend/src/app/pages/product/category/category.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AddProductComponent } from '../add-product/add-product.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { IUploadPhoto } from 'src/app/interfaces/IUpload-photo.interface';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -10,17 +10,19 @@ import { IUploadPhoto } from 'src/app/interfaces/IUpload-photo.interface';
 export class CategoryComponent extends AddProductComponent {
   @ViewChild('inputFile') private inputFile: ElementRef;
   public form: FormGroup;
-  public nameFiles: string;
   public filesThumb: Array<string> = [];
+  public placeHolderInputFile: string = 'Selecione uma Foto';
+  private listNameFiles: Array<string> = [];
+  private files: Array<File> = [];
   constructor(private readonly formBuilder: FormBuilder) {
     super();
   }
 
   override ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: [''],
-      description: [''],
-      photo: [''],
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      photo: ['', [Validators.required]],
     });
   }
 
@@ -39,8 +41,18 @@ export class CategoryComponent extends AddProductComponent {
     const reader = new FileReader();
     reader.onload = () => {
       this.filesThumb.push(reader.result as string);
-      console.log(this.filesThumb);
+      this.listNameFiles.push(file.name);
+      this.placeHolderInputFile = this.listNameFiles.join(', ');
+      this.files.push(file);
+      console.log(this.files);
     };
     reader.readAsDataURL(file);
+  };
+
+  public removeFiles = (): void => {
+    this.filesThumb = [];
+    this.listNameFiles = [];
+    this.placeHolderInputFile = 'Selecione uma foto';
+    this.form.value.photo = '';
   };
 }
