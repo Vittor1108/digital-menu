b059c62c4056daf239707dd123f546303f271870
Vittor Daniel <vittordaniel1108@gmail.com>
2023-01-24 21:19:51 -0300

    init fronted add employee

--- frontend/src/app/pages/employees/employees.component.ts
+++ frontend/src/app/pages/employees/employees.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
+  public filesThumbProduct: Array<string> = [];
+  public placeHolderInputFile: string = 'Seleciona uma foto';
+  public form: FormGroup;
+  public dropdownSettings: IDropdownSettings = {};
+  private listNameFiles: Array<string> = [];
+  private files: Array<File> = [];
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', []],
+      screens: ['', []],
+      email: ['', []],
+      password: ['', []],
+    });
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'id',
+      textField: 'name',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+      enableCheckAll: false,
+      searchPlaceholderText: 'Telas...',
+    };
   }
 
+  public getInfoImage = (event: Event): void => {
+    const file = (event.target as HTMLInputElement).files![0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.filesThumbProduct.push(reader.result as string);
+      this.listNameFiles.push(file.name);
+      this.placeHolderInputFile = this.listNameFiles.join(', ');
+      this.files.push(file);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  public changeImage = (image: number): void => {
+    const thumbImages = document.querySelectorAll<HTMLElement>('.thumb img');
+    const images = document.querySelectorAll<HTMLElement>('.slider > ul li');
+    images.forEach((e, index) => {
+      if (index === image) {
+        images[image].classList.remove('removeImage');
+        return;
+      }
+      e.classList.add('removeImage');
+    });
+
+    thumbImages.forEach((e, index) => {
+      if (index === image) {
+        thumbImages[image].classList.add('imageSelected');
+        return;
+      }
+      e.classList.remove('imageSelected');
+    });
+  };
+
+  public removeFiles = (): void => {
+    this.filesThumbProduct = [];
+    this.listNameFiles = [];
+    this.files = [];
+    this.placeHolderInputFile = 'Selecione uma foto';
+    this.form.value.photo = '';
+    const inputFile =
+      document.querySelector<HTMLInputElement>('input[type=file]');
+    inputFile!.value = '';
+  };
+
+  public addImage = (): void => {
+    document.querySelector<HTMLElement>('input[type=file]')!.click();
+  };
+
+  public onSubmit = (): void => {
+    console.log(this.form.value);
+  };
 }
