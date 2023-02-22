1ec50d9c1d5f8e451e7208d3523064524c9bfe19
Vittor Daniel <vittordaniel1108@gmail.com>
2023-02-22 19:51:46 -0300

    featured: task#018 menu finalizado

--- front-react/src/components/Nav/index.tsx
+++ front-react/src/components/Nav/index.tsx
@@ -13,6 +13,21 @@ import { FaBriefcase } from "react-icons/fa";
 import { BsFillBasket2Fill } from "react-icons/bs";
 
 export const Navigation = () => {
+  const showAccordion = (numberAccordion: number): void => {
+    const treeMenu = document.querySelectorAll(".treeMenu > ul");
+
+    if (treeMenu[numberAccordion].classList.contains("show")) {
+      treeMenu[numberAccordion].classList.remove("show");
+      return;
+    }
+
+    treeMenu.forEach((e) => {
+      e.classList.remove("show");
+    });
+
+    treeMenu[numberAccordion].classList.add("show");
+  };
+
   return (
     <>
       <NavigationMenu>
@@ -26,22 +41,47 @@ export const Navigation = () => {
               <p>Dashboard</p>
             </div>
           </li>
-          <li className="treeMenu">
+          <li className="treeMenu" onClick={() => showAccordion(0)}>
             <div>
               <FaArchive />
               <p>Menus</p>
             </div>
             <RiArrowDownSLine />
             <ul>
-              <li>ok</li>
+              <li>
+                <a href="">Adicionar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Editar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Adicionar Categorias</a>
+              </li>
+              <li>
+                <a href="">Editar Categorias</a>
+              </li>
             </ul>
           </li>
-          <li className="treeMenu">
+          <li className="treeMenu" onClick={() => showAccordion(1)}>
             <div>
               <BsFillBasket2Fill />
               <p>Ingredientes</p>
             </div>
             <RiArrowDownSLine />
+            <ul>
+              <li>
+                <a href="">Adicionar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Editar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Adicionar Categoria</a>
+              </li>
+              <li>
+                <a href="">Editar Categoria</a>
+              </li>
+            </ul>
           </li>
           <li>
             <div>
@@ -49,12 +89,26 @@ export const Navigation = () => {
               <p>Pedidos</p>
             </div>
           </li>
-          <li className="treeMenu">
+          <li className="treeMenu" onClick={() => showAccordion(2)}>
             <div>
               <FaUserFriends />
               <p>Funcionários</p>
             </div>
             <RiArrowDownSLine />
+            <ul>
+              <li>
+                <a href="">Adicionar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Editar Prdouto</a>
+              </li>
+              <li>
+                <a href="">Adicionar Categoria</a>
+              </li>
+              <li>
+                <a href="">Editar Categoria</a>
+              </li>
+            </ul>
           </li>
           <li>
             <div>
--- front-react/src/components/Nav/styles.ts
+++ front-react/src/components/Nav/styles.ts
@@ -10,14 +10,14 @@ export const NavigationMenu = styled.nav`
   top: 0;
   box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
 
-  ul {
-    li {
-      padding: 15px 0 15px 25px;
+  > ul {
+    > li {
+      padding: 15px 0;
       font-size: 15px;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      margin-bottom: 10px;
+      transition: color 0.3s;
       &:nth-child(1) {
         text-align: center;
         background-color: ${colors.graySecondary};
@@ -33,6 +33,8 @@ export const NavigationMenu = styled.nav`
       > div {
         display: flex;
         align-items: center;
+        padding-left: 15px;
+        margin-bottom: 10px;
 
         p {
           margin-left: 10px;
@@ -44,21 +46,43 @@ export const NavigationMenu = styled.nav`
       }
 
       &:hover {
-        color: red;
-        transition: all 0.3s;
+        color: ${colors.redPrimaryColor};
         cursor: pointer;
       }
     }
 
     .treeMenu {
-      border: 1px solid red;
       flex-wrap: wrap;
       ul {
         width: 100%;
-        &:nth-child(1) {
-          background-color: none;
+        padding-top: 10px;
+        max-height: 0;
+        transform: rotateX(90deg);
+        transition: transform 0.3s;
+        li {
+          background-color: ${colors.graySecondary};
+          padding: 20px 0 20px 45px;
+          margin: 0;
+          transition: color 0.3s;
+          a {
+            color: #000000de;
+            font-size: 14px;
+            transition: color 0.3s;
+            &:hover {
+              color: ${colors.redPrimaryColor};
+            }
+          }
+
+          &:nth-child(1) {
+            text-align: left;
+          }
         }
       }
     }
+  }
+
+  .show {
+    transform: rotateX(0) !important;
+    max-height: 300px !important;
   }
 `;
