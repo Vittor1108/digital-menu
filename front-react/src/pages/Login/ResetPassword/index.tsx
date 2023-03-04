968973e10f32b1b5b5aefa1cff9b57b5310f88ab
Vittor Daniel <vittordaniel1108@gmail.com>
2023-03-03 23:27:32 -0300

    .

---
+++ front-react/src/pages/Login/ResetPassword/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { Form } from "react-router-dom";
+export const ResetPassword = (): JSX.Element => {
+  return (
+    <Container maxW="466px" width="100%" border="1px solid red">
+        <Form>
+
+        </Form>
+    </Container>
+  );
+};
